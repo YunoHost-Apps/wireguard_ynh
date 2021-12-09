c01884cf2f4161bf52a34a542ff8701648598cbd
ericgaspar <junk.eg@free.fr>
2021-12-09 12:31:02 +0100

    4.3

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -68,30 +68,6 @@ ynh_install_repo_gpg () {
 # EXPERIMENTAL HELPERS
 #=================================================
 
-# Check the architecture
-#
-# example: architecture=$(ynh_detect_arch)
-#
-# usage: ynh_detect_arch
-#
-# Requires YunoHost version 2.2.4 or higher.
-
-ynh_detect_arch(){
-	local architecture
-	if [ -n "$(uname -m | grep arm64)" ] || [ -n "$(uname -m | grep aarch64)" ]; then
-		architecture="arm64"
-	elif [ -n "$(uname -m | grep 64)" ]; then
-		architecture="amd64"
-	elif [ -n "$(uname -m | grep 86)" ]; then
-		architecture="386"
-	elif [ -n "$(uname -m | grep arm)" ]; then
-		architecture="arm"
-	else
-		architecture="unknown"
-	fi
-	echo $architecture
-}
-
 # Send an email to inform the administrator
 #
 # usage: ynh_send_readme_to_admin --app_message=app_message [--recipients=recipients] [--type=type]
