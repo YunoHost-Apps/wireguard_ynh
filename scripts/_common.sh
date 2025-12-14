593a522294e6e6e100846dba47fd35cd5450af06
oleole39 <59071673+oleole39@users.noreply.github.com>
2025-12-14 02:52:06 +0100

    shorten reload instruction

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -6,15 +6,15 @@
 
 _ynh_config_add_nftables() {
     ynh_add_config --template="nftables.conf" --destination="/etc/nftables.d/${app}.conf"
-    ynh_systemd_action --service_name=nftables.service --action=reload --line_match="Reloaded nftables.service"
+    ynh_systemd_action --service_name=nftables.service --action=reload
 }
 
 _ynh_config_remove_nftables() {
     ynh_secure_remove "/etc/nftables.d/${app}.conf"
-    ynh_systemd_action --service_name=nftables.service --action=reload --line_match="Reloaded nftables.service"
+    ynh_systemd_action --service_name=nftables.service --action=reload
 }
 
 _ynh_config_restore_nftables() {
     ynh_restore_file --origin_path="/etc/nftables.d/${app}.conf"
-    ynh_systemd_action --service_name=nftables.service --action=reload --line_match="Reloaded nftables.service"
+    ynh_systemd_action --service_name=nftables.service --action=reload 
 }
