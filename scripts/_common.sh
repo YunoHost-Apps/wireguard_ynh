134b5c415a50123547a73f6a933b05ddeeab47d6
Thomas <51749973+Thovi98@users.noreply.github.com>
2026-01-02 19:27:36 +0100

    service_name -> service

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -6,15 +6,15 @@
 
 _ynh_config_add_nftables() {
     ynh_config_add --template="nftables.conf" --destination="/etc/nftables.d/${app}.conf"
-    ynh_systemctl --service_name=nftables.service --action=reload
+    ynh_systemctl --service=nftables.service --action=reload
 }
 
 _ynh_config_remove_nftables() {
     ynh_safe_rm "/etc/nftables.d/${app}.conf"
-    ynh_systemctl --service_name=nftables.service --action=reload
+    ynh_systemctl --service=nftables.service --action=reload
 }
 
 _ynh_config_restore_nftables() {
     ynh_restore --origin_path="/etc/nftables.d/${app}.conf"
-    ynh_systemctl --service_name=nftables.service --action=reload 
+    ynh_systemctl --service=nftables.service --action=reload 
 }
