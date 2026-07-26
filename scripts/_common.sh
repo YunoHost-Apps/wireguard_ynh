f7001e6db56d23773460bd65de595bc98affc298
tituspijean <tituspijean@yunohost.org>
2026-07-26 23:07:34 +0200

    enh(scripts): cleanup and move system variables to _common.sh

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -16,5 +16,8 @@ _ynh_config_remove_nftables() {
 
 _ynh_config_restore_nftables() {
     ynh_restore "/etc/nftables.d/${app}.conf"
-    ynh_systemctl --service=nftables.service --action=reload 
+    ynh_systemctl --service=nftables.service --action=reload
 }
+
+interface=$(ip route | awk '/default/ { print $5 }' | head -n1)
+main_domain=$(cat /etc/yunohost/current_host)
